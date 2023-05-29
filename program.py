4afc513b632e3211e6cc9b01973f58a2bf263bf6
Sayalivadghule <126447963+Sayalivadghule@users.noreply.github.com>
2023-05-29 19:46:44 +0530

    Create program.py

---
+++ program.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform addition
+result = num1 + num2
+
+# Print the result
+print("Sum: ", result)
